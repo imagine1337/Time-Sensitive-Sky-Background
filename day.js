d22af4746ccf682512839ac375897cb47d2415d5
imagine <88944530+imagine1337@users.noreply.github.com>
2022-02-19 14:27:24 -0600

    removed console.log thing

--- day.js
+++ day.js
@@ -1,4 +1,3 @@
 var c = new Date().getHours();
-console.log(c)
 var sky = document.getElementById("day");
-sky.style.backgroundImage = `url(day/${Math.ceil(c/2)}.gif)`
+sky.style.backgroundImage = `url(day/${Math.ceil(c/2)}.gif)`;
